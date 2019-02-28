ba327b6dafd70155894a4b583bedff236c66693b
Hannah Vaughn <hvaughn618@gmail.com>
2019-02-27 20:59:04 -0600

    added for loop to date locations and changed CSS styling

--- index.js
+++ index.js
@@ -32,19 +32,80 @@ function navBarFunction() {
   }
 }
 
-function targetListen() {
-  var targetLink = document.getElementById('target-link');
-  targetLink.addEventListener('click', (clickedElement)=> {
-    var x = targetLink.getAttribute("href");
-    console.log(x);
-    var target = document.querySelector(x);
-    var scrollSettings = {
-      behavior: 'smooth',
-      block: 'start',
-    };
-    target.scrollIntoView(scrollSettings);
-    clickedElement.preventDefault();
-  });
+// function targetListen() {
+//   var targetLink = document.getElementById('target-link');
+//   targetLink.addEventListener('click', (clickedElement)=> {
+//     var x = targetLink.getAttribute("href");
+//     console.log(x);
+//     var target = document.querySelector(x);
+//     var scrollSettings = {
+//       behavior: 'smooth',
+//       block: 'start',
+//     };
+//     target.scrollIntoView(scrollSettings);
+//     clickedElement.preventDefault();
+//   });
+// }
+// // document.onLoad = targetListen;
+// window.onload = targetListen;
+
+function colorButton() {
+  var c = document.getElementById('color');
+  var color = c.value;
+  var textContent = document.getElementById('content');
+  // console.log(textContent.innerHTML);
+  if (color === "blue") {
+    textContent.innerHTML = "Roses are red, water is blue, rain is icky, but you are not.";
+  }
+  if (color === "red") {
+    textContent.innerHTML = "Valentine's Day? Schmalentine's Day...";
+  }
+  if (color === "yellow") {
+    textContent.innerHTML = "Your future is so bright you need shades!";
+  }
+  if (color === "mocha-tan") {
+    textContent.innerHTML = "Not your average pickup line: 'Your skin reminds me of mocha...wanna grab coffee?'";
+  }
 }
-// document.onLoad = targetListen;
-window.onload = targetListen;
+
+function dateLocation() {
+  var l = document.getElementById('dates');
+  var locationString = l.value;
+  var textContent = document.getElementById('location-content');
+  textContent.innerHTML = "My thoughts?";
+  var locations = locationString.split(" ");
+  var ul = document.createElement('ul');
+  for (let i = 0; i < locations.length; i++) {
+    var li = document.createElement('li');
+    // console.log(locations[i]);
+    switch (locations[i]) {
+      case 'movies':
+        li.innerHTML = "Let's go to the movies!";
+        break;
+      case 'dinner':
+        li.innerHTML = "Let's grab dinner!";
+        break;
+      case 'coffee':
+        li.innerHTML = "Coffee sounds perfect!";
+        break;
+      case 'bowling':
+        li.innerHTML = "I love bowling!";
+        break;
+      default:
+        li.innerHTML = `You can't afford ${locations[i]}. Select another option.`;
+        li.style.color = 'red';
+        break;
+    }
+    ul.appendChild(li);
+  }
+  textContent.appendChild(ul);
+}
+
+var myData = {
+  head: {
+    title: "My Title"
+  },
+  body: {
+    p: "my paragaph"
+  }
+}
